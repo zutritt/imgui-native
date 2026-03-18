1680f5fa1e18b1b8b45a5d561788bbb53ab2b8be
ErnstStavroBlofeld <philipp@basz.uk>
2026-03-18 17:58:50 +0100

    Minor updates

--- gen/main.py
+++ gen/main.py
@@ -1,3 +1,7 @@
+# Here generate C++ files from lib/bindings/dcimgui.json file
+# Wrap C structs and functions in napi and expose them to JavaScript
+# Output files to lib/gen/napi
+# We also at the same time generate .d.ts file for our napi module
 
 if __name__ == "__main__":
     pass
