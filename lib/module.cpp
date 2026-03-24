cf2252c4112133ea929303c981d936b633c22c93
ErnstStavroBlofeld <philipp@basz.uk>
2026-03-24 21:41:02 +0100

    Remove old stuff

--- lib/module.cpp
+++ lib/module.cpp
@@ -1,23 +1,7 @@
 #include <napi.h>
-#include "dcimgui.h"
-#include "imgui.h"
-
-#include "types.h"
-#include "types.cpp"
-#include "refs.h"
-#include "refs.cpp"
-#include "gen/napi/callbacks.cpp"
-#include "gen/napi/structs.h"
-#include "gen/napi/structs.cpp"
-#include "gen/napi/functions.cpp"
-#include "gen/napi/enums.cpp"
 
 Napi::Object Init(Napi::Env env, Napi::Object exports)
 {
-  InitRefs(env, exports);
-  InitStructs(env, exports);
-  InitFunctions(env, exports);
-  InitEnums(env, exports);
   return exports;
 }
 
--- lib/types.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <napi.h>
-#include "dcimgui.h"
-
-ImVec2 ExtractImVec2(const Napi::Value& v);
-ImVec4 ExtractImVec4(const Napi::Value& v);
-ImColor ExtractImColor(const Napi::Value& v);
-ImTextureRef ExtractImTextureRef(const Napi::Value& v);
