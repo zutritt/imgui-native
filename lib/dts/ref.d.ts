f2a9782ecd830ea448c40ff89e75453872bfc8a5
ErnstStavroBlofeld <philipp@basz.uk>
2026-03-26 00:25:23 +0100

    Hand written ref types

---
+++ lib/dts/ref.d.ts
@@ -0,0 +1,40 @@
+export class BoolRef {
+  constructor();
+  constructor(value: boolean);
+
+  value: boolean;
+};
+
+export class IntRef {
+  constructor();
+  constructor(value: number);
+
+  value: number;
+}
+
+export class FloatRef {
+  constructor();
+  constructor(value: number);
+
+  value: number;
+}
+
+export class DoubleRef {
+  constructor();
+  constructor(value: number);
+
+  value: number;
+}
+
+export class StringRef {
+  constructor();
+  constructor(value: string);
+
+  value: string;
+
+  resize(newCapacity: number | bigint): void;
+}
+
+export class CallbackRef<CallbackType extends (...args: unknown[]) => unknown> {
+  constructor(callback: CallbackType);
+}
